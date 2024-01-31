0e7a1bdc3f6956f37919745ce911627dcfa768f1
Crynowecer0 <crynowecer11@gmail.com>
2024-01-30 19:41:38 -0500

    add profile image to Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,39 @@
-Camran
+<script></script>
+
+<main class="flex flex-column flex-1 p-4 mx-auto">
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Camran </span> Rynowecer
+        <br />Full Stack
+        <span class="poppins text-violet-400">Developer </span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My Favorite tech stack includes....
+      </p>
+      <a
+        class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+        href="#"
+        target="_blank"
+      >
+        <div
+          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        />
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow 2xl grid place-items-center">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img
+        src={"images/profile.jpg"}
+        alt="profile image"
+        class="object-cover z-[2] max-h-[70vh]"
+      />
+    </div>
+  </section>
+</main>
